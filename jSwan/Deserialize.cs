6954001e81083b848b1a43f44adbf26826d86cd3
andrewheumann <andheum@gmail.com>
2019-09-27 13:18:45 +0200

    support for direct file reading

--- jSwan/Deserialize.cs
+++ jSwan/Deserialize.cs
@@ -9,6 +9,7 @@ using Grasshopper.Kernel.Parameters;
 using System.Linq;
 using GH_IO.Serialization;
 using Grasshopper.Kernel.Types;
+using System.IO;
 
 // In order to load the result of this wizard, you will also need to
 // add the output bin/ folder of this project to the list of loaded
@@ -82,6 +83,13 @@ namespace jSwan
         {
             var json = "";
             if (!DA.GetData("JSON", ref json)) return;
+
+            //handle intaking a filepath as well as raw json
+            if(File.Exists(json))
+            {
+                json = File.ReadAllText(json);
+            }
+
 
             var deserialized = DeserializeToObject(json);
             if(deserialized == null)
